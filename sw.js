6cd2c439b000a74316b4877e5d6c5a58f885078a
Klasovic7 <nt@bs-objektservice.de>
2026-09-09 22:29:42 +0200

    Update sw.js
    Denk gleich! 250 Fragen

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // Service Worker: macht die App installierbar und offline startfähig.
 //
-// WICHTIG bei Änderungen: die Zahl in CACHE_NAME hochzählen (z. B. v29 -> v30).
+// WICHTIG bei Änderungen: die Zahl in CACHE_NAME hochzählen (z. B. v30 -> v31).
 // Nur dann wirft der Browser den alten Zwischenspeicher weg und alle Spieler
 // bekommen zuverlässig die neue Version. Das ist der einzige Handgriff, den man
 // nach dem Bearbeiten von Dateien nicht vergessen darf.
-const CACHE_NAME = "partyspiele-v29";
+const CACHE_NAME = "partyspiele-v30";
 
 const DATEIEN = [
   "./",
