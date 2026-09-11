3c1325f1bc0741b2fe52f9e1ed417a4f1e932e7b
Klasovic7 <nt@bs-objektservice.de>
2026-09-11 23:29:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Nur dann wirft der Browser den alten Zwischenspeicher weg und alle Spieler
 // bekommen zuverlässig die neue Version. Das ist der einzige Handgriff, den man
 // nach dem Bearbeiten von Dateien nicht vergessen darf.
-const CACHE_NAME = "partyspiele-v64";
+const CACHE_NAME = "partyspiele-v66";
 
 const DATEIEN = [
   "./",
