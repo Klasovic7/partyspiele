dfdf1507753d0c46a83b9545e90ffdf1a3381c74
Klasovic7 <nt@bs-objektservice.de>
2026-09-13 11:26:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // Service Worker: macht die App installierbar und offline startfähig.
 //
-// WICHTIG bei Änderungen: die Zahl in CACHE_NAME hochzählen (z. B. v78 -> v79).
+// WICHTIG bei Änderungen: die Zahl in CACHE_NAME hochzählen (z. B. v79 -> v80).
 // Nur dann wirft der Browser den alten Zwischenspeicher weg und alle Spieler
 // bekommen zuverlässig die neue Version. Das ist der einzige Handgriff, den man
 // nach dem Bearbeiten von Dateien nicht vergessen darf.
-const CACHE_NAME = "partyspiele-v79";
+const CACHE_NAME = "partyspiele-v80";
 
 const DATEIEN = [
   "./",
