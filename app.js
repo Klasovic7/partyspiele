9027a6a383e719cd27ecf9190c33997a79878355
Klasovic7 <nt@bs-objektservice.de>
2026-09-13 11:25:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import {
 } from "./kern/ui.js";
 import { SPIELE, spielInfo } from "./spiele/register.js";
 
-export const APP_VERSION = "v79";
+export const APP_VERSION = "v80";
 const appVersion = document.getElementById("app-version");
 appVersion.textContent = "Version " + APP_VERSION;
 
